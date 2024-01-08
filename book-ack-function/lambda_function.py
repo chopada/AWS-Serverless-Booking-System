e08193bbf2f27423c1623fc8df6f644e0d4e1e02
chopada <chopada.tushar@gmail.com>
2024-01-08 10:12:08 +0000

    catching variables from api gateway to lambda function

--- book-ack-function/lambda_function.py
+++ book-ack-function/lambda_function.py
@@ -1,7 +1,20 @@
-import requests
+import json
 
 def lambda_handler(event, context):
-    return {
+    # Assuming the query parameters are present in the 'queryStringParameters' field
+    query_params = event.get('queryStringParameters', {})
+
+    # Extracting values for ID and count
+    id_value = query_params.get('id', None)
+    count_value = query_params.get('count', None)
+
+    # Returning a response with extracted values
+    response = {
         'statusCode': 200,
-        'body': 'Your Booking is Started Successfully'
+        'body': json.dumps({
+            'id': id_value,
+            'count': count_value
+        })
     }
+
+    return response
